687a396c6eef63be4831259fc284fc632cd3f372
nadancho <nathan846@revature.net>
2023-10-25 19:42:43 -0400

    1

--- server/server.mjs
+++ server/server.mjs
@@ -6,12 +6,7 @@ import records from "./routes/record.mjs";
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cors(
-  {
-    
-  }
-
-));
+app.use(cors());
 app.use(express.json());
     
 
