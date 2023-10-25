251e9d889c390ce1ad4e88f3bd0f5c4a58a890f9
nadancho <nathan846@revature.net>
2023-10-25 18:19:09 -0400

    index.mjs

---
+++ server/index.mjs
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import "./loadEnvironment.mjs";
+import records from "./routes/record.mjs";
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors(
+  {
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  }
+
+));
+app.use(express.json());
+
+app.use("/ticket", records);
+
+// start the Express server
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
