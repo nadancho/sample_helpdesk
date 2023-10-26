3b9fd9916cc58cceb25b7609e0d069ca913784ae
nadancho <nathan846@revature.net>
2023-10-25 20:21:41 -0400

    exp

--- server/index.mjs
+++ server/index.mjs
@@ -1,22 +1,25 @@
 import express from "express";
 import cors from "cors";
-import "../loadEnvironment.mjs";
+import "../loadEnvironment.mjs"
 import records from "../routes/record.mjs";
+
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
-app.use(cors(
-  {
-    origin: ["https://deploy-mern-1whq.vercel.app"],
-    methods: ["POST", "GET"],
-    credentials: true,
-  }
-
-));
+app.use(cors());
 app.use(express.json());
 
+
+app.get("/", (req, res) => { 
+  res.send("Hello!"); 
+}); 
+
 app.use("/ticket", records);
+
+app.get("*", (req, res) => { 
+  res.send("PAGE NOT FOUND"); 
+});
 
 // start the Express server
 app.listen(PORT, () => {
